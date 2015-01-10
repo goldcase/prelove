97c0fad7edac58b1bf9db00e1622dbd74da7e219
Johnny Chang <johnny@johnnychang.com>
2015-01-10 01:07:16 -0800

    added return segue and new controllers

---
+++ prelove/AddLoveItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddLoveItemViewController.h
+//  prelove
+//
+//  Created by Johnny Chang on 1/10/15.
+//  Copyright (c) 2015 Johnny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddLoveItemViewController : UIViewController
+
+@end
---
+++ prelove/AddLoveItemViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddLoveItemViewController.m
+//  prelove
+//
+//  Created by Johnny Chang on 1/10/15.
+//  Copyright (c) 2015 Johnny Chang. All rights reserved.
+//
+
+#import "AddLoveItemViewController.h"
+
+@interface AddLoveItemViewController ()
+
+@end
+
+@implementation AddLoveItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
